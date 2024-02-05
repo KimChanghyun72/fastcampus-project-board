553eea0e019211864950ce511b3551a887b5b816
Kim Changhyun <ok2qkq@naver.com>
2024-02-05 13:12:03 +0900

    #19 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정.
    지금은 테스트 개발 단계고, 아직 인증 기능 구현 단계에 이르지 않았으므로,
    모든 페이지는 인증을 하지 않으며, form login을 활성화해 기본 로그인 폼이 생성.
    설정은 해당 커밋만 롤백 등의 작업이 용이하도록 별도로 커밋.
    참고: https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter

---
+++ project-board/src/main/java/com/fasecampus/projectboard/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.fasecampus.projectboard.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@EnableWebSecurity -> spring boot에서 security 사용할 때는 사용할필요 없음.
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+}
