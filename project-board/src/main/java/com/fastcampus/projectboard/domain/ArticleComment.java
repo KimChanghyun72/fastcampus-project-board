e95984ce753bfda202fa02946abc8da57614fee0
Kim Changhyun <ok2qkq@naver.com>
2024-02-16 11:59:08 +0900

    #48-대댓글 도메인 적용

--- project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
+++ project-board/src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -4,8 +4,11 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.boot.actuate.endpoint.web.Link;
 
+import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @ToString(callSuper = true)
@@ -24,19 +27,34 @@ public class ArticleComment extends AuditingFields {
     @Setter @ManyToOne(optional = false) private Article article; // 게시글 (ID)
     @Setter @ManyToOne(optional = false) @JoinColumn(name = "userId") private UserAccount userAccount; // 유저 정보 (ID)
 
+    @Setter
+    @Column(nullable = true, updatable = false)
+    private Long parentCommentId;                                   // 부모 댓글 id
+
+    @ToString.Exclude
+    @OrderBy("createdAt ASC")
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL)     // 부모댓글이 지워지면 자식댓글은 cascade로 삭제.
+    private Set<ArticleComment> childComments = new LinkedHashSet<>();
+
     @Setter @Column(nullable = false, length = 500) private String content; // 본문
 
 
     protected ArticleComment() {}
 
-    private ArticleComment(Article article, UserAccount userAccount, String content) {
+    private ArticleComment(Article article, UserAccount userAccount, Long parentCommentId, String content) {
         this.article = article;
         this.userAccount = userAccount;
+        this.parentCommentId = parentCommentId;
         this.content = content;
     }
 
     public static ArticleComment of(Article article, UserAccount userAccount, String content) {
-        return new ArticleComment(article, userAccount, content);
+        return new ArticleComment(article, userAccount, null, content);
+    }
+
+    public void addChildComment(ArticleComment child) {
+        child.setParentCommentId(this.id);
+        this.getChildComments().add(child);
     }
 
     @Override
