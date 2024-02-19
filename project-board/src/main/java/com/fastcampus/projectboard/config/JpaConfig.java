787594865dfc1cb346e97700baea210ece6d8086
Kim Changhyun <ok2qkq@naver.com>
2024-02-19 12:36:57 +0900

    #64-기타 변경사항

--- project-board/src/main/java/com/fastcampus/projectboard/config/JpaConfig.java
+++ project-board/src/main/java/com/fastcampus/projectboard/config/JpaConfig.java
@@ -23,6 +23,7 @@ public class JpaConfig {
                 .map(Authentication::getPrincipal)
                 .map(BoardPrincipal.class::cast)
                 .map(BoardPrincipal::getUsername);
+        // 기존의 로직은 인증완료된 Principal에서 username을 가져와야 하기 때문에 회원가입할 때 유저정보를 입력 불가능.
     }
 
 }
