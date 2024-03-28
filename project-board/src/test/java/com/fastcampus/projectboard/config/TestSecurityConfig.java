34fc2e01412e2d74c727ae3f4d857f82df4c9cae
Kim Changhyun <ok2qkq@naver.com>
2024-03-28 20:11:48 +0900

    테스트에서 발생한 오류 수정
    1. 테스트에 사용되는 스프링 시큐리티 설정인 TestSecurityConfig의 Service관련 내용이 적용되지 않은 오류를 수정
    2. 컨트롤러에서 @PathVariable, @RequestParm 에 url로 넘겨받은 인자의 값을 매핑하지 않아서 발생하는 테스트 오류 수정
    참고: https://www.inflearn.com/questions/1087879/pathvariable-name-%EC%83%9D%EB%9E%B5-%EC%A7%88%EB%AC%B8-%EB%93%9C%EB%A6%BD%EB%8B%88%EB%8B%A4

--- project-board/src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
+++ project-board/src/test/java/com/fastcampus/projectboard/config/TestSecurityConfig.java
@@ -1,28 +1,58 @@
 package com.fastcampus.projectboard.config;
 
 import com.fastcampus.projectboard.domain.UserAccount;
+import com.fastcampus.projectboard.dto.UserAccountDto;
 import com.fastcampus.projectboard.repository.UserAccountRepository;
+import com.fastcampus.projectboard.service.UserAccountService;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.event.annotation.BeforeTestMethod;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @Import(SecurityConfig.class)
+@TestConfiguration
 public class TestSecurityConfig {
-    @MockBean private UserAccountRepository userAccountRepository;
 
-    @BeforeTestMethod   // spring에서 지원하는 @BeforeTestMethod 사용.
-    public void securitySetUp() {
-        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+    @MockBean private UserAccountService userAccountService;
+
+    @BeforeTestMethod
+    public void securitySetup() {
+        given(userAccountService.searchUser(anyString()))
+                .willReturn(Optional.of(createUserAccountDto()));
+        given(userAccountService.saveUser(anyString(), anyString(), anyString(), anyString(), anyString()))
+                .willReturn(createUserAccountDto());
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
                 "unoTest",
                 "pw",
-                "uno-test@email.com",
-                "uno-test",
-                "test memo"
-        )));
+                "uno@mail.com",
+                "Uno",
+                "memo",
+                LocalDateTime.now(),
+                "uno",
+                LocalDateTime.now(),
+                "uno"
+        );
     }
+//    @MockBean private UserAccountRepository userAccountRepository;
+//
+//    @BeforeTestMethod   // spring에서 지원하는 @BeforeTestMethod 사용.
+//    public void securitySetUp() {
+//        given(userAccountRepository.findById(anyString())).willReturn(Optional.of(UserAccount.of(
+//                "unoTest",
+//                "pw",
+//                "uno-test@email.com",
+//                "uno-test",
+//                "test memo"
+//        )));
+//    }
 }
